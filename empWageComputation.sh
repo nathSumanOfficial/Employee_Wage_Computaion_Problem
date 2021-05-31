0892af8ab7ac2242f083fc7bf9b37bad2db380a6
Suman Nath <nathsuman1099@gmail.com>
2021-05-31 18:30:37 +0530

    Added: Attendence Check

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo $'\n'"Welcome to Employee Wage Computation Program on Master Branch"
+
+isPresent=$(( RANDOM%2 ))
+
+if [[ $isPresent -eq 1 ]]; then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
