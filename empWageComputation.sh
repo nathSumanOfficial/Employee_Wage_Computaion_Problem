82b2936ae60d309b83f47011f7cd2bc4b5ef0d59
Suman Nath <nathsuman1099@gmail.com>
2021-05-31 18:43:02 +0530

    Added: Full Day Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,9 +3,13 @@
 echo $'\n'"Welcome to Employee Wage Computation Program on Master Branch"
 
 isPresent=$(( RANDOM%2 ))
+wagePerHr=20
+fulldayHr=8
 
 if [[ $isPresent -eq 1 ]]; then
 	echo "Employee is Present"
 else
 	echo "Employee is Absent"
 fi
+
+fulldayWage=$(($wagePerHr*fulldayHr))
