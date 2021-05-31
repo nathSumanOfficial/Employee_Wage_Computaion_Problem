39f5d7939755bc5fc14bf107739b1f7e78927c52
Suman Nath <nathsuman1099@gmail.com>
2021-05-31 18:58:04 +0530

    Part time Wage Added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,7 +5,7 @@ echo $'\n'"Welcome to Employee Wage Computation Program on Master Branch"
 isPresent=$(( RANDOM%2 ))
 wagePerHr=20
 fulldayHr=8
-
+partdayHr=4
 fullPart=$((RANDOM%2))
 
 if [[ $isPresent -eq 1 ]]; then
@@ -15,5 +15,7 @@ else
 fi
 
 if [[ $fullPart -eq 1 ]]; then
-	fulldayWage=$(($wagePerHr*fulldayHr))
+	fulldayWage=$(($wagePerHr*$fulldayHr))
+elif [[ $fullPart -eq 0 ]]; then
+	partdayWage=$(($wagePerHr*$partdayHr))
 fi
